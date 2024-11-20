789416edb357f2f39e2c6aecb11dd14dcb22b373
annaravazzi <anna.carolina.ravazzi@gmail.com>
2024-11-20 15:39:17 -0300

    latency issues fixed

--- lib/serial.py
+++ lib/serial.py
@@ -1,22 +1,25 @@
 from machine import Pin, UART
 
 class Serial:
-    def __init__(self, channel, tx, rx, baudrate=9600, timeout=1000):
-        self.serial = UART(channel, baudrate=baudrate, tx=Pin(tx), rx=Pin(rx))
-        self.serial.init(baudrate=baudrate, timeout=timeout)
+    def __init__(self, channel, tx, rx, baudrate=9600, timeout=100):
+        self.serial = UART(channel, baudrate=baudrate, tx=Pin(tx), rx=Pin(rx), timeout=timeout)
+        # self.serial.init(baudrate=baudrate, timeout=timeout)
     
     def write(self, data):
-        self.serial.write(bytes(data))
+        self.serial.write(data)
     
-    def read(self, strip=True):
-        if strip:
-            return self.serial.read().decode().strip()
-        return self.serial.read().decode()
+    def read(self):
+        tmp = self.serial.read()
+        if tmp:
+            return tmp.decode('utf-8').strip()
+        return None
     
 if __name__ == "__main__":
     from time import sleep
     serial = Serial(0, 16, 17)
-    serial.write("Hello from Pico\n")
+    serial.write(bytes("Hello from Pico\n", 'utf-8'))
     while True:
-        serial.read()
+        read = serial.read()
+        if read:
+            print(read.decode('utf-8')) # type: ignore
         sleep(1)
